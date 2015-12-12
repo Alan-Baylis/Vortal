eb05e7f3d8d997b8d1ec0ee6e3062fa36066cec2
Owen Shartle <Owen@psu.edu>
2015-12-12 02:39:56 -0500

    Vortal - Minor Additions
    Just a few small changes..

--- Vortal/Assets/Scripts/Linecast.cs
+++ Vortal/Assets/Scripts/Linecast.cs
@@ -9,6 +9,8 @@ public class Linecast : MonoBehaviour {
     public bool grabby;
     public GameObject carriedObject;
     public float grabbedTime;
+    private GameObject objectHit;
+
 
 	// Use this for initialization
 	void Start ()
@@ -20,14 +22,18 @@ public class Linecast : MonoBehaviour {
 	void Update () {
         linestart = transform.position - new Vector3(0, 0.5f, 0);
         line.SetPosition(0, linestart);
-        if (Physics.Raycast(linestart, transform.forward, out hit, 5))
+        if (Physics.Raycast(linestart, transform.forward, out hit, 2))
         {
             line.SetPosition(1, hit.point);
+            line.SetWidth(0,0);
+            objectHit = hit.transform.gameObject;
             if(Input.GetKeyDown(KeyCode.E) && (hit.transform.tag == "Grabbable") && !grabby)
             {
                 carriedObject = hit.transform.gameObject;
                 carriedObject.GetComponent<Rigidbody>().useGravity = false;
                 carriedObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                carriedObject.GetComponent<Rigidbody>().freezeRotation = true;
+                carriedObject.GetComponent<Rigidbody>().isKinematic = true;
                 grabby = true;
                 grabbedTime = Time.time;
                 carriedObject.transform.SetParent(transform);
@@ -41,8 +47,10 @@ public class Linecast : MonoBehaviour {
         if(grabby && Input.GetKeyDown(KeyCode.E) && (Time.time - grabbedTime > 0.1))
         {
             carriedObject.GetComponent<Rigidbody>().useGravity = true;
+            carriedObject.GetComponent<Rigidbody>().freezeRotation = false;
+            carriedObject.GetComponent<Rigidbody>().isKinematic = false;
             grabby = false;
-            carriedObject.transform.SetParent(null);            
+            carriedObject.transform.SetParent(null);
         }
     }
 }
